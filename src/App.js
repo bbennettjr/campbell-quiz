67c76cca58001a1b43a08f190257ec2d93499713
Bradford Bennett Jr <bbennettjr@gmail.com>
2017-03-04 14:00:10 -0500

    Add QuizBanner and CSS link for react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { PageHeader } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
+import { Label } from 'react-bootstrap';
+
+// QuizBanner
+function QuizBanner(props) {
+  return (
+    <PageHeader>Campbell Quiz <small>Test your knowledge of this <label bsClass="label" bsStyle="danger">magnetic</label> family</small></PageHeader>
+    );
+}
+// Tab Navigation
 
 class App extends Component {
   render() {
@@ -13,6 +24,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <QuizBanner/>
+        <Button bsStyle="info" bsSize='large'>Danger</Button>
       </div>
     );
   }
